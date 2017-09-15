dae1224b81be0d6bfac330526569d99d9d75a826
mdahus <md.ashfaq.h@gmail.com>
2017-09-15 16:00:51 +0530

    first version of code

---
+++ Hello.java
@@ -0,0 +1,9 @@
+class Hello{
+public static void main(String[] args){
+System.out.println("Testing Docker");
+System.out.println("With java app.");
+System.out.println("compile and run java app.");
+System.out.println("new line added.");
+System.out.println("new second line added.");
+}
+}
